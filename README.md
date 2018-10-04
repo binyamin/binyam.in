a118cf655d26e0175a434e7e3fc604da4a50eec5
Binyamin <bgreen@oblivy.com>
2018-10-04 13:46:57 -0400

    Make mobile friendly
    Add Media Queries

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Portfolio-B- The second half of a Split A|B test for my personal portfolio website.
+# Portfolio-B
+*The second half of a Split A|B test for my personal portfolio website*
+
+## Technologies Used:
+* HTML
+* CSS
+* Javascript
+* jQuery
--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
 /*-------Modules-------
   - Navbar underline effect
+  - Smooth scroll effect
+  - Responsive Navbar & hamburger animation
+  - Update copyright year
 */
 
 // Navbar underline effect
-
 const navlink = document.querySelectorAll('.nav-link');
 
 navlink.forEach(function(e) {
     e.addEventListener('click', function(){
-        navlink.forEach( a => a.classList.remove('active'));
-        this.classList.toggle('active');
-    })
+        navlink.forEach( a => a.classList.remove('current'));
+        this.classList.toggle('current');
+   	})
 })
+
+ //Smooth scroll effect
+  var scrollLink = $('.scroll');
+    scrollLink.click(function(e){
+       e.preventDefault(); 
+      $('body,html').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 1000);
+   });
+
+//Responsive Navbar
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav');
+
+hamburger.addEventListener('click', function(){
+    this.classList.toggle('responsive');
+    navMenu.classList.toggle('activeMenu');
+ })
+
+//Copyright Year
+const copyYear = document.querySelector('.copy');
+
+const now = new Date();
+copyYear.innerHTML = now.getFullYear();
