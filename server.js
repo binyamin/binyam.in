6903327a296fd8f2a3fe89ac2e0b03ca6e221395
Binyamin Green <binyaminagreen@gmail.com>
2019-05-20 22:20:06 -0400

    trouubleshoot

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const app = express()
 let ical = require('ical-generator')
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded())
