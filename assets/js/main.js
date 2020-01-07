91d27e451d4714058193883f6f8d2bfc48caebd7
Binyamin Aron Green <binyaminagreen@gmail.com>
2020-01-07 14:57:16 -0500

    finish site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,1 @@
-document.querySelectorAll('.panel-tab-js').forEach(a => {
-    a.addEventListener('click', ev => {
-        ev.preventDefault();
-        document.querySelectorAll('.panel').forEach(p => p.hidden = true)
-        document.getElementById(a.dataset.href).hidden = false;
-    })
-})
+MicroModal.init();
