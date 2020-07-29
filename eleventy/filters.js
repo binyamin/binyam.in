403dd4c4fbb78f9839d34c85f830cfc52af77b02
Binyamin Aron Green <binyamingreen@protonmail.com>
2020-07-29 16:44:21 -0400

    sort books by title

--- eleventy/filters.js
+++ eleventy/filters.js
@@ -1,3 +1,5 @@
+const { arrayReplaceAt } = require("markdown-it/lib/common/utils");
+
 module.exports = (eleventyConfig, md) => {
     eleventyConfig.addFilter("absolute_url", value => {
         return "https://binyam.in" + (value.startsWith("/") ? value : "/" + value);
@@ -5,6 +7,29 @@ module.exports = (eleventyConfig, md) => {
     
     eleventyConfig.addFilter("titlecase", str => {
         return titleCase(str);
+    })
+
+    function alphabetaSort (left, right) {
+        // Thanks to Sindre Sorhus for the logic here
+
+        const collator = new Intl.Collator();
+        const compare = (left, right) => left === right ? 0 : collator.compare(left, right);
+
+        function caselessCompare(left, right) {
+            const lowercaseComparison = compare(left.toLowerCase(), right.toLowerCase());
+            return lowercaseComparison === 0 ? compare(left, right) : lowercaseComparison;
+        }
+
+        function articleCompare(left, right) {
+            const articleComparison = caselessCompare(left.replace(/^(a|an|the)\s/i, ""), right.replace(/^(a|an|the)\s/i, ""));
+            return articleComparison === 0 ? caselessCompare(left, right) : articleComparison;
+        }
+        
+        return articleCompare(left, right);
+    }
+
+    eleventyConfig.addFilter("sort_ab", (arr, key) => {
+        return arr.sort((a,b) => alphabetaSort(a[key], b[key]));
     })
     
     eleventyConfig.addFilter("slugify", str => {
