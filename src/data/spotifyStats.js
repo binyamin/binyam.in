279f5f6d8bdf0ac5957d7c47e3e9faf8488ba771
Binyamin Aron Green <binyamingreen@protonmail.com>
2020-09-23 20:00:36 -0400

    compute pretty song duration; limit results

--- src/data/spotifyStats.js
+++ src/data/spotifyStats.js
@@ -2,18 +2,19 @@ const datacache = require("@binyamin/data-cache");
 
 module.exports = function() {
     const data = datacache.get("spotify.tracks");
-    
+
     return data.items.map(entry => ({
         title: entry.name,
         link: entry.external_urls.spotify,
         type: entry.type,
         artists: entry.artists.map(a => a.name),
         duration_ms: entry.duration_ms,
+        duration: `${Math.floor(entry.duration_ms / 60000)}:${Math.round((entry.duration_ms / 1000) % 60)}`,
         id: entry.id,
         album: {
             album_type: entry.album.album_type.toLowerCase(),
             title: entry.album.name,
             album_art: Object.fromEntries(entry.album.images.map(i => [i.height, i.url])),
         }
-    }));
+    })).slice(0, 10);
 }
