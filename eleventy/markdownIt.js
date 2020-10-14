575730a178779280b1e2fdf1816e4deb88e1fa5d
Binyamin Aron Green <binyamingreen@protonmail.com>
2020-10-14 12:30:32 -0400

    implement wikilinks without npm module

--- eleventy/markdownIt.js
+++ eleventy/markdownIt.js
@@ -15,31 +15,32 @@ const markdownItOptions = {
                 '</code></pre>';
             } catch (__) {}
         }
-        
+
         return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
     }
 };
-
-const mdWikilinksOptions = {
-    baseURL: "/notes/",
-    makeAllLinksAbsolute: true,
-    uriSuffix: "/",
-    linkPattern: /\[\[([\w\s/-]+)(\|([\w\s/]+))?\]\]/,
-    postProcessPageName: (pageName) => {
-        return pageName.trim().toLowerCase().replace(/\s/g, "-");
-    }
-}
 
 const md = markdownIt(markdownItOptions)
     .use(require('markdown-it-footnote'))
     .use(require('markdown-it-attrs'))
     .use(require('markdown-it-abbr'))
-    .use(require("@kwvanderlinde/markdown-it-wikilinks")(mdWikilinksOptions))
     .use(function(md) {
         md.linkify.add("#", {
-            validate: /[\w-]+/,
+            validate: /^[\w-]+/,
             normalize: match => {
                 match.url = "/notes/search?tag=" + match.raw.slice(1);
+            }
+        })
+    })
+    .use(function(md) {
+        // Recognize Mediawiki links ([[text]])
+        md.linkify.add("[[", {
+            validate: /^([\w\s/-]+)(.\w+)?\s?(\|\s?([\w\s/]+))?\]\]/,
+            normalize: match => {
+                const parts = match.raw.slice(2,-2).split("|");
+                parts[0] = parts[0].replace(/.(md|markdown)\s?$/i, "");
+                match.text = (parts[1] || parts[0]).trim();
+                match.url = `/notes/${parts[0].trim()}/`;
             }
         })
     })
