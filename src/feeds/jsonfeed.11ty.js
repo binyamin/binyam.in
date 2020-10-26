3f67fa48cd9dc6a34ae87ba5a214440cc0747ed9
Binyamin Aron Green <binyamingreen@protonmail.com>
2020-10-26 11:22:48 -0400

    add json feed

---
+++ src/feeds/jsonfeed.11ty.js
@@ -0,0 +1,49 @@
+/**
+ * JSON feed for https://binyam.in
+ * @see https://jsonfeed.org/version/1.1 (docs)
+ */
+
+// Note: If necessary (size>=1mb), implement pagination
+
+class Feed {
+    data() {
+        return {
+            permalink: "/feed.json",
+            eleventyExcludeFromCollections: true
+        }
+    }
+
+    render(data) {
+        return JSON.stringify({
+            version: "https://jsonfeed.org/version/1.1",
+            title: "Binyamin Green's Blog",
+            home_page_url: "https://binyam.in",
+            feed_url: "https://binyam.in/feed.json",
+            description: data.site.desc,
+            icon: "https://binyam.in/assets/logo.png",
+            favicon: "https://binyam.in/assets/logo.png",
+            language: "en-US",
+            authors: [
+                {
+                    name: data.site.author.name,
+                    url: data.site.url,
+                    avatar: "https://binyam.in/assets/img/profile@512.jpeg"
+                }
+            ],
+            items: data.collections.posts.map(p => ({
+                id: this.absolute_url(p.url),
+                url: this.absolute_url(p.url),
+                title: p.data.title,
+                content_html: this.escape_once(p.templateContent),
+                summary: p.data.desc || "No description provided",
+                date_published: p.data.date,
+                ...(p.data.modified ? {date_modified: p.data.modified} : {}),
+                ...(p.data.tags ? {tags: p.data.tags} : {})
+            }))
+        }, null, 4);
+    }
+}
+
+
+
+module.exports = Feed;
