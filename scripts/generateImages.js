27264320af9a581e2febc763de8866fdcc5b71d9
Binyamin Aron Green <binyamingreen@protonmail.com>
2021-01-05 14:34:53 -0500

    fix twitter images

--- scripts/generateImages.js
+++ scripts/generateImages.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const yaml = require("js-yaml");
 
-const runIf = (process.env.CI || process.env.DEBUG === "generateimages");
+const runIf = (process.env.CI || process.env.DEBUG === "imgscript");
 
 function createImage( title, outPath=__dirname ) {
     const {createCanvas, loadImage, registerFont} = require("canvas");
