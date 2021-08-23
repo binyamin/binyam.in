d0827095f99c521d17d7fe30f080aba53e3795a0
Binyamin Aron Green <binyamingreen@protonmail.com>
2021-08-23 00:11:31 -0400

    reorganize filters, and add one

--- eleventy/filters.js
+++ eleventy/filters.js
@@ -4,6 +4,15 @@ module.exports = (eleventyConfig, md) => {
     eleventyConfig.addFilter("absolute_url", slug => {
         return "https://binyam.in" + (slug.startsWith("/") ? slug : "/" + slug);
     })
+    
+    const filters = {
+        ...require("./date_filter"),
+        ...require("./sort_ab_filter")
+    }
+
+    for (const fn in filters) {
+        eleventyConfig.addFilter(fn, filters[fn])
+    }
 
     eleventyConfig.addFilter("escape_once", str => {
         // From <https://github.com/harttle/liquidjs/blob/master/src/builtin/filters/html.ts>
@@ -39,42 +48,6 @@ module.exports = (eleventyConfig, md) => {
 
     eleventyConfig.addFilter("includes", (arr, value) => {
         return arr.includes(value);
-    })
-
-
-    eleventyConfig.addFilter("date_est", (datetime, time=true, format) => {
-        const estFormat = new Intl.DateTimeFormat("en-US", {
-            timeZone: "America/New_York",
-            dateStyle: format === "short" ? "short" : "long",
-            ...(time ? { timeStyle: "long" } : null),
-        });
-
-        const dt = estFormat.format(new Date(datetime));
-
-        return dt.replace(/:\d{2}([\s\w]+)$/, "$1"); // remove seconds
-    })
-
-    function alphabetaSort (left, right) {
-        // Thanks to Sindre Sorhus for the logic here
-
-        const collator = new Intl.Collator();
-        const compare = (left, right) => left === right ? 0 : collator.compare(left, right);
-
-        function caselessCompare(left, right) {
-            const lowercaseComparison = compare(left.toLowerCase(), right.toLowerCase());
-            return lowercaseComparison === 0 ? compare(left, right) : lowercaseComparison;
-        }
-
-        function articleCompare(left, right) {
-            const articleComparison = caselessCompare(left.replace(/^(a|an|the)\s/i, ""), right.replace(/^(a|an|the)\s/i, ""));
-            return articleComparison === 0 ? caselessCompare(left, right) : articleComparison;
-        }
-
-        return articleCompare(left, right);
-    }
-
-    eleventyConfig.addFilter("sort_ab", (arr, key) => {
-        return arr.sort((a,b) => alphabetaSort(a[key], b[key]));
     })
 
     eleventyConfig.addFilter("slugify", str => {
