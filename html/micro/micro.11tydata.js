708e6f7b76ce2d992ea9a950ccad71befc58118c
Binyamin Aron Green <binyamingreen@protonmail.com>
2021-12-21 11:32:58 -0500

    microblog assets should not render

--- html/micro/micro.11tydata.js
+++ html/micro/micro.11tydata.js
@@ -2,7 +2,7 @@ const isMarkdown = (data) => data.page.inputPath.endsWith("md");
 
 module.exports = {
     eleventyComputed: {
-        permalink: (d) => isMarkdown(d) ? `/p/${d.page.fileSlug}/` : false,
+        permalink: (d) => isMarkdown(d) ? `/p/${d.page.fileSlug}/` : undefined,
         category: "micro",
         layout: (d) => isMarkdown(d) ? "post" : "",
         eleventyExcludeFromCollections: (d) => isMarkdown(d) ? false : true,
