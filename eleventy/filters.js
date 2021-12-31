b2925c410cf7b77f7030094c30a88c0090c0bdc6
Binyamin Aron Green <binyamingreen@protonmail.com>
2021-12-30 22:31:36 -0500

    move layouts & includes to njk

--- eleventy/filters.js
+++ eleventy/filters.js
@@ -72,6 +72,8 @@ module.exports = (eleventyConfig, md) => {
         }
         if(format === "rfc" || format === "string") {
             return dayjs(timestamp).toString();
+        } else if (format == 'iso') {
+            return dayjs(timestamp).toISOString();
         } else {
             return dayjs(timestamp).format(format);
         }
