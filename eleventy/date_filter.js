47953d5c05510150bd2647b506d856ebd42441a4
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-01-02 12:57:18 -0500

    remove code from extra filter

--- eleventy/date_filter.js
+++ eleventy/date_filter.js
@@ -1,30 +1,3 @@
-function _date(datetime=new Date(), format="iso") {
-    if(datetime === "now") {
-        datetime = new Date();
-    }
-    if (format === "iso") {
-        return datetime.toISOString();
-    } else if (format === "human") {
-        return datetime.toLocaleString("en-US", {
-            dateStyle: "long",
-            timeStyle: "long"
-        }).replace(/:\d{2}([\s\w]+)$/, "$1"); // remove seconds
-    } else if (format === "human-short") {
-        if((new Date()).getFullYear() === datetime.getFullYear()) {
-            return datetime.toLocaleDateString("en-US", {
-                month: "short",
-                day: "numeric"
-            });
-        } else {
-            return datetime.toLocaleDateString("en-US", {
-                dateStyle: "medium"
-            });
-        }
-    } else {
-        throw new Error(`desired format "${format}" not recognized`);
-    }
-}
-
 function date_est(datetime, time=true, format) {
     const estFormat = new Intl.DateTimeFormat("en-US", {
         timeZone: "America/New_York",
@@ -37,4 +10,4 @@ function date_est(datetime, time=true, format) {
     return dt.replace(/:\d{2}([\s\w]+)$/, "$1"); // remove seconds
 }
 
-module.exports = {_date, date_est};
+module.exports = {date_est};
