70ea05b4d98228c4db5dbb0072b0f808f5222019
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-01-12 13:54:05 -0500

    add timezone support to dayjs

--- utils/date-njk.js
+++ utils/date-njk.js
@@ -1,17 +1,30 @@
 const dayjs = require("dayjs");
-const dayjsAdvancedFormat = require("dayjs/plugin/advancedFormat");
 
-dayjs.extend(dayjsAdvancedFormat);
+const advancedFormat = require("dayjs/plugin/advancedFormat");
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
 
-module.exports = function date(timestamp, format, _kwargs) {
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(advancedFormat);
+
+dayjs.tz.setDefault("America/New_York");
+
+module.exports = function date(timestamp, format, kwargs={}) {
     if(timestamp == "now" || !timestamp) {
         timestamp = new Date();
     }
+
+    let dt = dayjs(timestamp);
+
+    const tz = kwargs.tz || kwargs.timezone;
+    if(tz) dt = dt.tz(tz);
+
     if(format === "rfc" || format === "string") {
-        return dayjs(timestamp).toString();
+        return dt.toString();
     } else if (format == 'iso') {
-        return dayjs(timestamp).toISOString();
+        return dt.toISOString();
     } else {
-        return dayjs(timestamp).format(format);
+        return dt.format(format);
     }
 }
