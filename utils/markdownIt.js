509ae4ffd636a9533062fbcf7627f66023d533d5
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-04-24 17:57:13 -0400

    bring back header id slugs

--- utils/markdownIt.js
+++ utils/markdownIt.js
@@ -1,18 +1,24 @@
 // Custom Markdown parser
+/** @type {import("highlight.js").default} */
 const hljs = require('highlight.js');
 
 const markdownIt = require('markdown-it');
+
+/** @type {import("markdown-it-anchor").default} */
+const anchor = require("markdown-it-anchor");
+
+/** @type {import("markdown-it").Options} */
 const markdownItOptions = {
     html: true,
     linkify: true,
-    highlight: function (str, lang) {
+    highlight(str, lang) {
         if (lang && hljs.getLanguage(lang)) {
             try {
-                return `<pre class="hljs code-block" data-lang="${hljs.getLanguage(lang).name}"><code>` +
-                hljs.highlight(str, {
+                return `<pre class="hljs code-block" data-lang="${hljs.getLanguage(lang).name}"><code>`
+                + hljs.highlight(str, {
                     language: lang
-                }).value +
-                '</code></pre>';
+                }).value
+                + '</code></pre>';
             } catch (__) {}
         }
 
@@ -25,6 +31,7 @@ const md = new markdownIt(markdownItOptions)
     .use(require('markdown-it-attrs'), {
         allowedAttributes: ['id', 'class', 'rel', /^data-.+$/]
     })
+    .use(anchor, { level: 2 })
     .use(require("markdown-it-external-anchor"), { domain: 'binyam.in' })
     .use(require("@binyamin/markdown-it-wikilinks"), {
         base: '/wiki/'
