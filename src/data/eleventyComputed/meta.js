3049791dfc62cc934080f209c08b2386d8531653
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-08-01 21:19:54 -0400

    replace custom eleventy-version data with builtin

--- src/data/eleventyComputed/meta.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    eleventyVersion: data => {
-        const {devDependencies: a, dependencies: b} = data.pkg;
-        const deps = Object.assign({}, a, b);
-        return deps['@11ty/eleventy']?.replace("^", "");
-    }
-}
