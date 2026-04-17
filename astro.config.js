34affe56ed585a1c85b4ceee02ad37fc0381b2f1
Binyamin Aron Green <binyamingreen@protonmail.com>
2026-04-17 15:28:45 -0400

    chore: migrate base layout

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import { site } from '~/site.config';
 
 export default defineConfig({
+	site: site.url,
 	server: {
 		port: 3000,
 	},
---
+++ src/site.config.ts
@@ -0,0 +1,10 @@
+export const me = {
+	name: 'Binyamin Green',
+	twitter: 'binyamingreen',
+};
+
+export const site = {
+	url: 'https://binyam.in',
+	title: me.name,
+	description: 'Binyamin Green is a UI Designer and Front-end Developer.',
+};
