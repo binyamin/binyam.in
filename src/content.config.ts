327e09b804a298d3ab855c3cae5d0ea7dc36672c
Binyamin Aron Green <binyamingreen@protonmail.com>
2026-04-19 12:41:55 -0400

    chore(setup): configure content collections

---
+++ src/content.config.ts
@@ -0,0 +1,31 @@
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
+import {
+	type BaseSchema,
+	defineCollection,
+	type SchemaContext,
+} from 'astro:content';
+
+const Post: (ctx: SchemaContext) => BaseSchema = ({ image }) =>
+	z.object({
+		title: z.string().nonempty(),
+		description: z.string().nonempty().optional(),
+		date: z.coerce.date(),
+		modified: z.coerce.date().optional(),
+		thumbnail: image().optional(),
+		tags: z.array(z.string().nonempty()).min(1).optional(),
+		folders: z.array(z.string().nonempty()).min(1).optional(),
+		elsewhere: z.array(z.httpUrl()).min(1).optional(),
+	}).strict();
+
+const blog = defineCollection({
+	loader: glob({ base: './content/blog', pattern: '**/*.md' }),
+	schema: Post,
+});
+
+const micro = defineCollection({
+	loader: glob({ base: './content/micro', pattern: '**/*.md' }),
+	schema: Post,
+});
+
+export const collections = { blog, micro };
