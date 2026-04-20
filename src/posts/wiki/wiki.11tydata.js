e2e7b5df65268f231a122e8a9fcc0eb519c84eb4
Binyamin Aron Green <binyamingreen@protonmail.com>
2026-04-20 15:08:31 -0400

    chore: remove wiki

--- src/posts/wiki/wiki.11tydata.js
+++
@@ -1,9 +0,0 @@
-import {titleCase} from "title-case";
-
-export default {
-	layout: "note",
-	category: "wiki",
-	eleventyComputed: {
-		title: data => titleCase(data.title || data.page.fileSlug),
-	}
-}
