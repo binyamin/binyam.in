7098b3409103d7d56a9e78709cb3be782d60f5b7
Binyamin Aron Green <binyamingreen@protonmail.com>
2026-04-24 13:50:31 -0400

    chore: migrate webmentions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,3 +19,43 @@ export function dateEst(
 
 	return dt.replace(/:\d{2}([\s\w]+)$/, '$1'); // remove seconds
 }
+
+/**
+ * Remove html tags from a string
+ */
+
+export function striptags(html: string): string {
+	return html.replaceAll(/<\/?[^>]+\/?>/g, '');
+}
+
+type StringLike = string | { toString(): string };
+
+export function groupBy<
+	T extends Record<string, StringLike>,
+	K extends keyof T | (string & {}),
+>(
+	data: T[],
+	key: K | ((item: T) => K | null),
+): Record<
+	T[K] extends string ? Extract<T[K], string>
+		: K extends string ? K
+		: never,
+	T[]
+> {
+	return data.reduce((prev, curr) => {
+		if (typeof key === 'function') {
+			const k = key(curr);
+
+			if (k) {
+				prev[k] ??= [];
+				prev[k].push(curr);
+			}
+		} else {
+			const k = curr[key].toString();
+			prev[k] ??= [];
+			prev[k].push(curr);
+		}
+
+		return prev;
+	}, {} as Record<any, T[]>);
+}
